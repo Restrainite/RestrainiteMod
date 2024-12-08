c18cb0533949689a3b2acca76fdc2f046c5be982
SnepDrone <189016534+SnepDrone@users.noreply.github.com>
2024-12-08 07:50:16 +0100

    v0.3.5 bump version

--- Restrainite/RestrainiteMod.cs
+++ Restrainite/RestrainiteMod.cs
@@ -13,7 +13,7 @@ public class RestrainiteMod : ResoniteMod
 
     public override string Name => "Restrainite";
     public override string Author => "SnepDrone Zenuru";
-    public override string Version => "0.3.4";
+    public override string Version => "0.3.5";
     public override string Link => "https://github.com/SnepDrone/Restrainite";
 
     /**
